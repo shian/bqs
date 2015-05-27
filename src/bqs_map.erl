6e88906787dde308506e6bdd1450c370b62a395a
shian <shian5@gmail.com>
2015-05-27 22:40:41 +0800

    read map data into ets

--- src/bqs_map.erl
+++ src/bqs_map.erl
@@ -24,7 +24,7 @@
          terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
-
+-define(PROPERTY(X), ets:lookup_element(map1, {property, X}, 2)).
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -35,19 +35,29 @@ start_link(FilePath) ->
 %%% Game API
 %%%===================================================================
 get_startingAreas() ->
-    gen_server:call(?SERVER, get_startingAreas).
+    %% TODO random pickup
+    ets:lookup_element(map1, {start_area, 1}, 2).
 
 is_colliding(X, Y) ->
-    gen_server:call(?SERVER, {is_colliding, X, Y}).
+    TileId = pos_to_tileid(X, Y),
+    ets:member(map1, {collision, TileId}).
 
 is_out_of_bounds(X, Y) ->
-    gen_server:call(?SERVER, {is_out_of_bounds, X, Y}).
+    Width = ?PROPERTY(width),
+    Height = ?PROPERTY(height),
+    (X < 1) or (X >= Width) or (Y < 1) or (Y >= Height).
 
+%% TileId starts at 1 and maps to {0,0}
+%% The TileId 0 is invalid because of this
+tileid_to_pos(0) ->
+    exit("Invalid TileId");
 tileid_to_pos(TileId) ->
-    gen_server:call(?SERVER, {tileid_to_pos, TileId}).
+    Width = ?PROPERTY(width),
+    tileid_to_pos(TileId, Width).
 
 pos_to_tileid(X, Y) ->
-    gen_server:call(?SERVER, {pos_to_tileid, {X, Y}}).
+    Width = ?PROPERTY(width),
+    (Y * Width) + X + 1.
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -56,18 +66,6 @@ init([MapName]) ->
     {ok, Map} = read_map(MapName),
     {ok, Map}.
 
-handle_call(get_startingAreas, _From, Map) ->
-    {reply, Map#map.startingAreas, Map};
-handle_call({is_colliding, X, Y}, _From, Map) ->
-    {reply, do_is_colliding(X, Y, Map), Map};
-handle_call({is_out_of_bounds, X, Y}, _From, Map) ->
-    {reply, do_is_out_of_bounds(X, Y, Map), Map};
-handle_call({tileid_to_pos, TileId}, _From, #map{width = Width} = Map) ->
-    Res = tileid_to_pos(TileId, Width),
-    {reply, Res, Map};
-handle_call({pos_to_tileid, {X, Y}}, _From, #map{width = Width} = Map) ->
-    Res = pos_to_tileid(X, Y, Width),
-    {reply, Res, Map};
 handle_call(Request, From, State) ->
     bqs_util:unexpected_call(?MODULE, Request, From, State),
     Reply = ok,
@@ -77,14 +75,6 @@ handle_cast(Msg, State) ->
     bqs_util:unexpected_cast(?MODULE, Msg, State),
     {noreply, State}.
 
-handle_info({read_map, MapName}, State) ->
-    lager:debug("read map ~p", [MapName]),
-    {ok, NewMap} = try read_map(MapName)
-                   catch Err:Why ->
-                           lager:error("read map: ~p", [{Err, Why, erlang:get_stacktrace()}]),
-                           {ok, State}
-                   end,
-    {noreply, NewMap};
 handle_info(Info, State) ->
     bqs_util:unexpected_info(?MODULE, Info, State),
     {noreply, State}.
@@ -98,41 +88,42 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
-do_is_colliding(X, Y, Map) ->
-    TileId = pos_to_tileid(X, Y, Map#map.width),
-    %% TODO, improve performance by putting collisions in an ets table and do a
-    %% ets lookup
-    lists:member(list_to_binary(integer_to_list(TileId)), Map#map.collisions).
-
-get_checkpoint({[{<<"id">>, Id},
+insert_checkpoint({[{<<"id">>, Id},
                  {<<"x">>, X}, {<<"y">>, Y},
                  {<<"w">>, W}, {<<"h">>, H},
-                 {<<"s">>, S}]}) ->
-    Start = bqs_util:integer_to_boolean(S),
-    #cp{id=Id, x=X, y=Y, w=W, h=H, s=Start}.
+                 {<<"s">>, S}]}, Tab) ->
+    case bqs_util:integer_to_boolean(S) of
+        true ->
+            ets:insert(Tab, {{check_point, Id}, #cp{id=Id, x=X, y=Y, w=W, h=H, s=true}}),
+            ets:insert(Tab, {{start_area, Id}, #cp{id=Id, x=X, y=Y, w=W, h=H, s=true}});
+        false ->
+            ets:insert(Tab, {{check_point, Id}, #cp{id=Id, x=X, y=Y, w=W, h=H, s=false}})
+    end.
 
-get_mobarea({[{<<"id">>, Id},
+insert_mobarea({[{<<"id">>, Id},
               {<<"x">>, X},
               {<<"y">>, Y},
               {<<"width">>, W},
               {<<"height">>, H},
               {<<"type">>, Type},
-              {<<"nb">>, Nb}]}) ->
-    #mobarea{id=Id,x=X,y=Y,w=W,h=H,type=Type,nb=Nb}.
+              {<<"nb">>, Nb}]}, Tab) ->
+    Mob = #mobarea{id=Id,x=X,y=Y,w=W,h=H,type=Type,nb=Nb},
+    ets:insert(Tab, {{mobarea, Id}, Mob}),
+    [add_mob(Mob)|| _ <- lists:seq(1, Nb)], %spawn the enemies
+    ok.
 
-get_staticEntity([], _) ->
-    [];
-get_staticEntity([{TileId, <<"rat">> = Type} | Entities], Width) ->
+insert_staticEntity([], _, _) ->
+    ok;
+insert_staticEntity([{TileId, <<"rat">> = Type} | Entities], Width, Tab) ->
     {X, Y} = tileid_to_pos(list_to_integer(binary_to_list(TileId)), Width),
     Id = random:uniform(1000) + length(Entities),
     Nb = 1,
-    [#mobarea{id = Id, type = Type, x = X, y = Y, w = X, h = Y, nb=Nb} |
-     get_staticEntity(Entities, Width)];
-get_staticEntity([_ | Entities], Width) ->
-    get_staticEntity(Entities, Width).
-
-do_is_out_of_bounds(X, Y, #map{height = Height, width = Width}) ->
-    (X < 1) or (X >= Width) or (Y < 1) or (Y >= Height).
+    Mob = #mobarea{id = Id, type = Type, x = X, y = Y, w = X, h = Y, nb=Nb},
+    ets:insert(Tab, {{static, Id}, Mob}),
+    add_mob(Mob),
+    insert_staticEntity(Entities, Width, Tab);
+insert_staticEntity([_ | Entities], Width, Tab) ->
+    insert_staticEntity(Entities, Width, Tab).
 
 %% TileId starts at 1 and maps to {0,0}
 %% The TileId 0 is invalid because of this
@@ -148,16 +139,6 @@ tileid_to_pos(TileId, Width) ->
     Y = (TileId - 1) div Width,
     {X, Y}.
 
-%% Calculates the TileId for pos X,Y
-pos_to_tileid(X, Y, Width) ->
-    (Y * Width) + X + 1.
-
-start_mob([]) ->
-    ok;
-start_mob([#mobarea{nb = Nb} = Mob | Tail]) ->
-    [add_mob(Mob)|| _ <- lists:seq(1, Nb)],
-    start_mob(Tail).
-
 add_mob(#mobarea{type = Type, x = X, y = Y, w = X, h = Y}) ->
     bqs_mob_sup:add_child(Type, X, Y);
 add_mob(#mobarea{type = Type, x = X, y = Y, w = W, h = H}) ->
@@ -167,29 +148,26 @@ add_mob(#mobarea{type = Type, x = X, y = Y, w = W, h = H}) ->
 
 read_map(MapName) ->
     File = filename:join([code:priv_dir(bqs), "maps", MapName]),
-    lager:debug("read map ~p", [File]),
     {ok, FileBin} = file:read_file(File),
     {Json} = jiffy:decode(FileBin),
+
+    Tab = ets:new(map1, [set, public, named_table, {write_concurrency, true}, {read_concurrency, true}]),
+
     Height = proplists:get_value(<<"height">>, Json),
     Width = proplists:get_value(<<"width">>, Json),
+    ets:insert(Tab, {{property, height}, Height}),
+    ets:insert(Tab, {{property, width}, Width}),
+
     Collisions = proplists:get_value(<<"collisions">>, Json),
-    Checkpoints = [get_checkpoint(Point) ||
-                      Point <- proplists:get_value(<<"checkpoints">>, Json)],
-    StartingAreas = [Cp || Cp <- Checkpoints, Cp#cp.s == true],
-    RoamingAreas = [get_mobarea(Area) ||
+    [ets:insert(Tab, {{collision, X}, 1}) || X <- Collisions],
+
+    [insert_checkpoint(Point, Tab) ||
+                     Point <- proplists:get_value(<<"checkpoints">>, Json)],
+
+    [insert_mobarea(Area, Tab) ||
                        Area <- proplists:get_value(<<"roamingAreas">>, Json)],
     {Entities} = proplists:get_value(<<"staticEntities">>, Json),
-    StaticEntities = get_staticEntity(Entities, Width),
+    insert_staticEntity(Entities, Width, Tab),
 
-                                                %spawn the enemies
-    start_mob(RoamingAreas),
-    start_mob(StaticEntities),
-
-    Map = #map{checkpoints = Checkpoints,
-               startingAreas = StartingAreas,
-               height= Height,
-               width = Width,
-               json = Json,
-               collisions = Collisions
-              },
-    {ok, Map}.
+    lager:debug("read map ~p", [File]),
+    {ok, Tab}.
