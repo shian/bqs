def04262959b73d7c7de4a5d0938bc9985ab5865
shian <shian5@gmail.com>
2015-05-28 09:52:21 +0800

    update start script

--- init.sh
+++ init.sh
@@ -1,1 +1,4 @@
-erl -pa deps/*/ebin -pa ../bqs/ebin -boot start_sasl -eval "application:ensure_all_started(bqs)"
+#!/usr/bin/env bash
+
+./rebar compile
+erl -pa deps/*/ebin -pa ../bqs/ebin -boot start_sasl -eval "application:ensure_all_started(bqs)" -bqs client_dir \"PATH_TO_BROWSERQUEST_CLIENT\"
